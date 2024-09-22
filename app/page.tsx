1715fdf81c79eb7c801a8a0338bccb2f33c41653
donaldliu1 <donaldliu1313@gmail.com>
2024-09-22 20:41:19 +0100

    basic front page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <p> Hi I&#39;m Donald</p>
+    <div className="flex justify-center items-center space-between gap-44 bg-[#f1edf8] h-screen">
+      <div>
+        <p className="text-7xl py-4"> Hi, I&#39;m Donald </p>
+        <p className="text-3xl"> Junior Software Developer </p>
+      </div>
+      <Image 
+        src="/cat.png"
+        width={390}
+        height={390}
+        alt='picture of cat :3'
+        >
+      </Image>
+    </div>
   );
 }
