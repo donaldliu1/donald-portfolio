22563fb839f6d8936c8964460a75bb58e1d9ecf7
donaldliu1 <donaldliu1313@gmail.com>
2024-09-26 22:02:14 +0100

    added animated bouncing arrow at the bottom of page

--- app/component/about.tsx
+++ app/component/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div id="about" className="snap-center space-between gap-44 bg-secondary h-screen text-textcolour">
-            <p className=" flex justify-center my-12"> About me</p>
+            <p className=" flex justify-center "> About me</p>
             <div>
                 <p> Younger </p>
                 <p> Education </p>
--- app/component/introduction.tsx
+++ app/component/introduction.tsx
@@ -1,19 +1,24 @@
 import Image from 'next/image'
+import Down from './down'
 
 export default function Introduction() {
-return (
-    <div id="introduction" className="snap-center flex justify-center items-center space-between gap-44 bg-background h-screen text-textcolour">
-        <div>
-          <p className="text-7xl py-4"> Hi, I&#39;m Donald </p>
-          <p className="text-3xl"> Junior Software Developer </p>
+  return (
+    <div id="introduction" className="snap-center flex flex-col justify-between items-center bg-background h-screen text-textcolour">
+      <div className="flex flex-col justify-center items-center flex-grow">
+        <div className="flex justify-between items-center gap-44">
+          <div>
+            <p className="text-7xl py-4"> Hi, I&#39;m Donald </p>
+            <p className="text-3xl"> Junior Software Developer </p>
+          </div>
+          <Image 
+            src="/cat.png"
+            width={390}
+            height={390}
+            alt='picture of cat :3'
+          />
         </div>
-        <Image 
-          src="/cat.png"
-          width={390}
-          height={390}
-          alt='picture of cat :3'
-          >
-        </Image>
+      </div>
+      <Down/>
     </div>
-)
+  )
 }
